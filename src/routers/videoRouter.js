2d52ef55d1a2861b351a90eb62cb2f0859eba49a
wonsss <bigdatajang@gmail.com>
2021-08-29 03:30:03 +0900

    14.complete

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,7 +7,7 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
-import { protectorMiddleware, videoUpload} from "../middlewares";
+import { protectorMiddleware, videoUpload } from "../middlewares";
 
 const videoRouter = express.Router();
 
@@ -25,6 +25,6 @@ videoRouter
   .route("/upload")
   .all(protectorMiddleware)
   .get(getUpload)
-  .post(videoUpload.single("video"),postUpload);
+  .post(videoUpload.fields([{ name: "video" }, { name: "thumb" }]), postUpload);
 
 export default videoRouter;
