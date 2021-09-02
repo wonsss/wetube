45433900c2dfbf8951ead3048de90f61d6186863
wonsss <bigdatajang@gmail.com>
2021-09-03 00:02:21 +0900

    isHeroku2

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.use("/videos", videoRouter);
 app.use("/api", apiRouter);
 
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.header("Cross-Origin-Resource-Policy", "cross-origin");
   next();
