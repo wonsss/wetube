7b890cf96cc5e6251ea7599deb5f60aec85c7424
wonsss <bigdatajang@gmail.com>
2021-09-02 18:44:53 +0900

    complete

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,21 @@
 import express from "express";
-import { registerView, createComment, handleDeleteComment } from "../controllers/videoController";
+import {
+  registerView,
+  createComment,
+  handleDeleteComment,
+} from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
-apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
-apiRouter.delete("/comments/:id([0-9a-f]{24})/delete", handleDeleteComment);
+apiRouter
+  .route("/videos/:id([0-9a-f]{24})/comment")
+  .all(protectorMiddleware)
+  .post(createComment);
+apiRouter
+  .route("/comments/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .delete(handleDeleteComment);
 
 export default apiRouter;
