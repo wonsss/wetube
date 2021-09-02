fe7c23cc1c984af3375d1b1ef1ecc571cde8f87e
wonsss <bigdatajang@gmail.com>
2021-09-03 00:33:55 +0900

    helmet

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
 import express from "express";
@@ -20,6 +21,16 @@ app.use(cors(corsOptions));
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    referrerPolicy: { policy: "strict-origin-when-cross-origin" },
+    crossOriginEmbedderPolicy: true,
+    crossOriginOpenerPolicy: "same-origin",
+  })
+);
+
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -43,10 +54,10 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 app.use("/api", apiRouter);
 
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "credentialless");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  res.header("Cross-Origin-Resource-Policy", "cross-origin");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Cross-Origin-Embedder-Policy", "credentialless");
+//   res.header("Cross-Origin-Opener-Policy", "same-origin");
+//   res.header("Cross-Origin-Resource-Policy", "cross-origin");
+//   next();
+// });
 export default app; //app을 export한다
